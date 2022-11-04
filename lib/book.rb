c8066907dbb9be8357f576b02b02f2e11472bd5b
Trey <trey.delcamp@gmail.com>
2022-11-04 12:17:42 -0500

    commit tree af1fc8bcbb9979ae66f0a6e9530e9af53f2577fc

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
+require 'pry'
+
+# Hmm Princeton why can I not combine these two parameters and invoke them like I did with the one below? OHHHH! I think it's because the test wants it done just a certain way. But I don't believe my way is wrong.
+
+class Book
+    attr_reader :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_accessor :turn_page #("Flipping the page...wow, you read fast!") Hmmm I want to figure out how to do it this way. My error: Failure/Error: expect { book.turn_page }.to output(a_string_matching("Flipping the page...wow, you read fast!")).to_stdout
+    # expected block to output a string matching "Flipping the page...wow, you read fast!" to stdout, but output nothing
+    # Diff:
+    # @@ -1 +1 @@
+    # -(a string matching "Flipping the page...wow, you read fast!")
+    # +""
+    #So the string is empty on my end, which is why I tried pushing a value through via a parameter in the accessor. Maybe that's not the best way to do this? PRINCETON! How is can we do this? Would we need to call an actual new book and implement that string there? Nope. because then it would have to be in our #initialize.. FINE I'll do it your long way! 
+    def initialize title #, author
+        @title = title
+        # @author = author
+    end
+    
+    # def author= author
+    #     @author= author
+    # end
+    # def author
+    #     @author
+    # end
+
+    # def page_count= number
+    #     @page_count= number
+    # end
+    # def page_count
+    #     @page_count
+    # end
+# Princeton's doing it the long way on purpose! Screw that.
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+# b1 = Book.new("And Then There Were None", "Agatha Christie") aww we didn't even need to do this?
 
 
+# binding.pry
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
+require 'pry'
 # Make your shoe class here!
+#Oh. I watched your video and I remembered you said we can list it. BTW Trey, Princeton left his puts string for tunr page, so probs have to do it that way here.
+
+class Shoe
+    attr_reader :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    
+    def initialize brand
+        @brand = brand
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+s1 = Shoe.new("Adidas")
